932c72e0d2546b088c6111b27b388e549d2b7e98
Aki- <akshaydalvi24@gmail.com>
2019-07-16 15:04:44 +0530

    Add project files.

---
+++ CustomException/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CustomException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name. (Other than Steve)");
+
+            string name = "";
+
+            try
+            {
+                name = Convert.ToString(Console.ReadLine());
+
+                if(name=="Steve")
+                {
+                    throw new SteveException("Enter other than Steve.");
+                }
+                else
+                {
+                    Console.WriteLine("Your name:- " + name);
+                    Console.Read();
+                }
+            }
+            catch(SteveException sE)
+            {
+                Console.WriteLine("SteveException fired !!");
+                Console.WriteLine(sE.Message);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            Console.Read();
+
+
+            
+
+        }
+    }
+}
